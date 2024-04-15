f95bfe4275212a5a227081eb199d0752c54a5536
Chris Nickerson <cnickerson2@me.com>
2024-04-15 09:01:13 -0600

    Finalized Crash Course Files

---
+++ HelloWorld/Data/DataContextEF.cs
@@ -0,0 +1,33 @@
+﻿using HelloWorld.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HelloWorld.Data
+{
+    public class DataContextEF : DbContext
+    {
+
+        private IConfiguration _config;
+        public DbSet<Computer>? Computer { get; set; }
+
+        public DataContextEF(IConfiguration config)
+        {
+            _config = config;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"),
+                    optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("TutorialAppSchema");
+            modelBuilder.Entity<Computer>().HasKey(c => c.ComputerId);
+                //.ToTable("Computer","TutorialAppSchema");
+        }
+    }
+}
