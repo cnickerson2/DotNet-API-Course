af9647220ce936fb8d06aa352951b62d1a79d48d
Chris Nickerson <cnickerson2@me.com>
2024-04-15 13:59:34 -0600

    Added UserController to test parameters

---
+++ DotnetAPI/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        public UserController() 
+        {
+
+        }
+
+        
+
+        [HttpGet("GetUsers/{testValue}")]
+        //public IActionResult Test()
+        public string[] GetUsers(string testValue)
+        {
+            string[] responseArray = new string[] { 
+                "test1",
+                "test2",
+                testValue
+            };
+            return responseArray;
+        }
+
+    }
+}
