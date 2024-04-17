43460d948f24caf27b65abadbe2eebce6366784c
Chris Nickerson <cnickerson2@me.com>
2024-04-17 17:48:13 -0600

    Added DTOs for Adding and Editing Posts

---
+++ DotnetAPI/DTOs/PostToAddDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetAPI.DTOs
+{
+    public partial class PostToAddDTO
+    {
+        public string PostTitle { get; set; } = "";
+        public string PostContent { get; set; } = "";
+    }
+}
---
+++ DotnetAPI/DTOs/PostToEditDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetAPI.DTOs
+{
+    public partial class PostToEditDTO
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; } = "";
+        public string PostContent { get; set; } = "";
+    }
+}
