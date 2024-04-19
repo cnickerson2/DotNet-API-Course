223cb6309fd46f2588e5e2d7193fd01bef8cf4ba
Chris Nickerson <cnickerson2@me.com>
2024-04-19 16:43:08 -0600

    Consolidated Endpoitnts into UserComplete

---
+++ DotnetAPI/Controllers/UserCompleteController.cs
@@ -0,0 +1,80 @@
+﻿using DotnetAPI.Data;
+using DotnetAPI.DTOs;
+using DotnetAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System;
+
+namespace DotnetAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserCompleteController : ControllerBase
+    {
+        DataContextDapper _dapper;
+
+        public UserCompleteController(IConfiguration config) 
+        {
+            _dapper = new DataContextDapper(config);
+        }
+
+        [HttpGet("TestConnection")]
+        public DateTime TestConnection()
+        {
+            return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+        }
+
+        [HttpGet("GetUsers/{userId}/{isActive}")]
+        public IEnumerable<UserComplete> GetUsers(int userId, bool isActive)
+        {
+            string sql = @"EXEC TutorialAppSchema.spUsers_Get";
+            string parameters = "";
+            if (userId != 0)
+            {
+                parameters += ", @UserId=" + userId.ToString();
+            }
+            if (isActive)
+            {
+                parameters += ", @Active=" + (isActive ? 1 : 0).ToString();
+            }
+
+            sql += parameters.Substring(1);
+            IEnumerable<UserComplete> users = _dapper.LoadData<UserComplete>(sql);
+            return users;
+        }
+
+        [HttpPut("UpsertUser")]
+        public IActionResult UpsertUser(UserComplete user)
+        {
+            string sql = "EXEC TutorialAppSchema.spUser_Upsert " +
+                         "SET @FirstName = '"+   user.FirstName +
+                         "', @LastName = '"+    user.LastName +
+                         "', @Email = '" +      user.Email +
+                         "', @Gender = '" +     user.Gender +
+                         "', @Active = '" + user.Active +
+                         "', @JobTitle = '" + user.JobTitle +
+                         "', @Department = '" + user.Department +
+                         "', @Salary = " + user.Salary +
+                         ", @UserId = " +  user.UserId;
+            if(_dapper.ExecuteSql(sql))
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to Update User");
+        }
+
+        [HttpDelete("DeleteUser/{userId}")]
+        public IActionResult DeleteUser(int userId)
+        {
+            string sql = "EXEC TutorialAppSchema.spUser_Delete @UserId=" + userId.ToString();
+
+            if (_dapper.ExecuteSql(sql))
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to Delete User");
+        }
+    }
+}
