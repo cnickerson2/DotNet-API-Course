cda77498985b537f2a7dbc2925d3e0f30f9e2152
Chris Nickerson <cnickerson2@me.com>
2024-04-22 12:20:13 -0600

    Updated DTO to acces JobTitle Department and Salary

--- DotnetAPI/DTOs/UserForRegistrationDTO.cs
+++ DotnetAPI/DTOs/UserForRegistrationDTO.cs
@@ -9,5 +9,8 @@
         public string LastName { get; set; } = "";
         public string Gender { get; set; } = "";
         public bool Active { get; set; }
+        public string JobTitle { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public decimal? Salary { get; set; }
     }
 }
